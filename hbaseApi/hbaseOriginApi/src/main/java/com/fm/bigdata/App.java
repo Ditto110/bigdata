29dc9dcdd635f688f5a5afc54043fa06260866b3
ditto <2293529964@qq.com>
2019-04-17 00:00:32 +0800

    【更新-hbaseOriginApi】

--- hbaseApi/hbaseOriginApi/src/main/java/com/fm/bigdata/App.java
+++ hbaseApi/hbaseOriginApi/src/main/java/com/fm/bigdata/App.java
@@ -2,10 +2,9 @@ package com.fm.bigdata;
 
 /**
  * Hello world!
- *
+ * maven program
  */
-public class App 
-{
+public class App {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
